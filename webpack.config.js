5da79c1e7191e94612ee29672f5db940d7e1ac73
Kamil Fus <balzac91@gmail.com>
2017-04-15 17:41:37 +0200

    setup webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,44 @@
-var path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 module.exports = function () {
-  var config = {};
+  const isBuild = process.env.npm_lifecycle_event === 'build';
+
+  const config = {};
 
   config.entry = './src/index.js';
 
   config.output = {
-    filename: 'bundle.js',
+    filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
   };
+
+  config.plugins = [];
+
+  config.plugins.push(
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new ProgressBarPlugin()
+  );
+
+
+  if (!isBuild) {
+    config.devtool = 'eval-source-map';
+  } else {
+    config.devtool = 'cheap-module-source-map';
+  }
+
+  if (!isBuild) {
+    config.devServer = {
+      contentBase: path.join(__dirname, 'src'),
+      historyApiFallback: true,
+      port: 8080,
+      inline: true,
+      stats: 'minimal'
+    };
+  }
 
   return config;
 };
